da753413d9f6aa432b166848f940e18c9b403e83
Sergey Rump <wnmsr3u@gmail.com>
2016-02-26 21:06:02 +0300

    Some fixes.

--- Timetable/BookmarksViewController.swift
+++ Timetable/BookmarksViewController.swift
@@ -22,26 +22,4 @@ class BookmarksViewController: SearchViewController
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
-    {
-        if(segue.identifier=="showTimetable")
-        {
-            if let vc=segue.destinationViewController as?CalendarViewController
-            {
-               vc.group=selectedGroup
-            }
-        }
-        super.prepareForSegue(segue,sender:sender)
-    }
 }
