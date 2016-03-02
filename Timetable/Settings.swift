aad7dd7998fbab946263fa4f0e6bfeb167b334e8
Sergey Rump <wnmsr3u@gmail.com>
2016-03-02 18:36:51 +0300

    Better dummy settings

--- Timetable/Settings.swift
+++ Timetable/Settings.swift
@@ -11,7 +11,45 @@ class Settings
 {
     static let instance=Settings()
     static var i:Settings{return instance}
+    let bundle=NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleIdentifier")!
+    let defaults = NSUserDefaults.standardUserDefaults()
+    func key(k:String)->String{return "\(bundle).\(k)"}
     
-    var showNotifications:Bool{return true}
-    var showNotificationOnUpdate:Bool{return false}
+    var showNotifications:Bool{
+        get{
+            return defaults.boolForKey(key("showNotifications"))
+        }
+        set{
+            defaults.setBool(newValue, forKey:key("showNotifications"))
+        }
+    }
+    var showNotificationOnUpdate:Bool{
+        get{
+            return defaults.boolForKey(key("showNotificationOnUpdate"))
+        }
+        set{
+            defaults.setBool(newValue, forKey:key("showNotificationOnUpdate"))
+        }
+    }
+    var firstLaunch:Bool{
+        get{
+            return !defaults.boolForKey(key("firstLaunch"))
+        }
+        set{
+            defaults.setBool(!newValue, forKey:key("firstLaunch"))
+        }
+    }
+    func reset()
+    {
+        if(firstLaunch)
+        {
+            showNotifications=true
+            showNotificationOnUpdate=false
+            firstLaunch=false
+        }
+    }
+    init()
+    {
+       reset()
+    }
 }
---
+++ Timetable/SettingsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SettingsViewController.swift
+//  Timetable
+//
+//  Created by Sergey Rump (SPHERE) on 02.03.2016.
+//  Copyright © 2016 spbstu. All rights reserved.
+//
+
+import Foundation
+class SettingsViewController: UIViewController
+{
+    @IBOutlet var notificationsSw:UISwitch!
+    @IBOutlet var notificationsOnUpdateSw:UISwitch!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        self.navigationItem.title="Настройки"
+        notificationsSw.on=Settings.i.showNotifications
+        notificationsOnUpdateSw.on=Settings.i.showNotificationOnUpdate
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        notificationsSw.on=Settings.i.showNotifications
+        notificationsOnUpdateSw.on=Settings.i.showNotificationOnUpdate
+    }
+    @IBAction func onSettingsChange(sender:AnyObject?)
+    {
+        Settings.i.showNotifications=notificationsSw.on
+        Settings.i.showNotificationOnUpdate=notificationsOnUpdateSw.on
+    }
+    
+    @IBAction func resetSettings(sender:AnyObject?)
+    {
+        Settings.i.firstLaunch=true
+        Settings.i.reset()
+        self.navigationController?.popViewControllerAnimated(true)
+        let n=LNNotification(title:"Настойки",message:"Настройки были сброшены")
+        LNNotificationCenter.defaultCenter().presentNotification(n, forApplicationIdentifier:"timetable")
+    }
+}
